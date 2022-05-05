1bcaac8996b170b8446b159d0c8c79123dc17e27
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-05-05 01:45:29 -0300

    Alterações de autenticação.

---
+++ src/config/auth.js
@@ -0,0 +1,9 @@
+
+const authConfig = {
+   // secretWord = 'grupo8HandOn';
+   secret : 'a57de912f38e8d41fce00f4d600c2caa4fea02c0',
+   expiresIn :'24h'    
+   
+}
+
+module.exports = authConfig;
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,26 +1,40 @@
+const jwt = require('jsonwebtoken');
+const auth = require('../config/auth');
 const  { Psicologos } = require("../models/");
 
 const LoginController = {
 
     async login(req, res) {
-        const { email,senha } = req.body;
-
+      const { email, password } = req.body;
         try {
           
-            const user = await Psicologos.findOne({where: {email,senha}});
+          const user = await Psicologos.findOne({where: {email, password}});
 
           if (!user) {
-            return res.status(401).json({ message: 'E-mail ou senha inválido, verifique e tente novamente'})
+            return res.status(401).json({ message: 'E-mail inválido, verifique e tente novamente'})
           }
-          
-          return res.status(200).json({user})
+          const {id ,name } = user;
+
+          return res.json({
+            user:{
+              id,
+              name,
+              email
+            },
+            token: jwt.sign({id},auth.secret, {
+              expiresIn:auth.expiresIn
+            })
+
+          })
 
         } catch (error) {
           return  res.status(400).send('Ocorreu algum erro. Entre em contato com o administrador do sistema');
         }
       },
 
-
+      checkPassword(password){
+        return bcrypt.compare(password, Psicologos.password)
+      }
 } 
 
 module.exports = LoginController;
