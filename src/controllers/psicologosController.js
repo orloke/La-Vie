0bed2ba7c2af5ef0e5c01c97494ef776c712fecc
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-05-06 09:31:47 -0300

    Correcao no listar por id para nao mostrar a senha

--- src/controllers/psicologosController.js
+++ src/controllers/psicologosController.js
@@ -15,16 +15,17 @@ const psicologosController = {
     listarPorID: async (req, res) => {
         const { id_psicologo } = req.params;
         try {
-            let filter = {
-                attributes: ['id_psicologo', 'nome', 'email', 'apresentacao'],
-            };
+            // let filter = {
+            //     attributes: ['id_psicologo', 'nome', 'email', 'apresentacao'],
+            // };
 
-            const listaPsicologos = await Psicologos.findOne({
+            const listaPsicologos = await Psicologos.findOne({                
+               attributes: {exclude: ['senha']},
                 where: {
                     status: 1,
                     id_psicologo,
                 },
-                filter,
+               // filter,
             });
 
             if (listaPsicologos) {
@@ -38,26 +39,38 @@ const psicologosController = {
 
     cadastrar: async (req, res) => {
         const { nome, email, senha, apresentacao } = req.body;
-
-        const novaSenha = bcrypt.hashSync(senha, 10);
-
-        const novoPsicologo = await Psicologos.create({
+    
+        try {
+          const findPsicologo = await Psicologos.findOne({ where: { email } });
+    
+          if(findPsicologo) {
+              return res.status(400).json("E-mail já cadastrado!")
+          }
+    
+          const novaSenha = bcrypt.hashSync(senha, 10);
+    
+          const novoPsicologo = await Psicologos.create({
             nome,
             email,
             senha: novaSenha,
             apresentacao,
-            status: 1,
-        });
-
-        res.status(201).json(novoPsicologo);
-    },
-
+          });
+    
+          res.status(201).json(novoPsicologo);
+        } catch (error) {
+          console.log(error);
+          res.status(400).json("Erro na requisição");
+        }
+      },
+    
     atualizar: async (req, res) => {
         const { id_psicologo } = req.params;
         const { nome, email, senha, apresentacao } = req.body;
 
         const novaSenha = bcrypt.hashSync(senha, 10);
-
+        if (!id_psicologo) {
+            return res.status(400).json("Informe o Id do Psicólogo");
+        }
         try {
             await Psicologos.update(
                 {
